71c519a996ea89a203f2176d2a8910066d60f89a
vaibhavmehtaa <vaibhav.mehta@vogo.in>
2019-08-25 14:34:19 +0530

    add new variables

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'iot_collector_app_kafka_consumer',
+    script: 'index.js',
+    instances: max,
+    autorestart: true,
+    watch: false,
+    cwd: '/opt/vogo/iot-collector-consumer',
+    exec_mode  : "cluster",
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
